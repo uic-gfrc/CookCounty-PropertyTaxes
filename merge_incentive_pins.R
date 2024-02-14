63abd035ea3ae9a72eb57499abf90a07a53dede6
AWM <awilbu2@uic.edu>
2024-02-13 22:56:51 -0600

    added 2021 commercial valuation data from CCAO. progress toward grouping by project.

--- merge_incentive_pins.R
+++ merge_incentive_pins.R
@@ -4,15 +4,79 @@ library(DBI)
 library(httr)
 library(jsonlite)
 library(glue)
-library(sf)
+# library(sf)
 library(readxl)
 
-incentive_pins <- read_csv("./Output/7_output_incentive_classes.csv")
+options(scipen = 999)
 
-access_db <- read_excel("incentivePINs_accessDB.xlsx")  %>% 
-  arrange((Status_cleaned) ) 
+# 3652 in 2021
+incentive_pins <- read_csv("./Output/7_output_incentive_classes.csv") %>%
+  mutate(#pin = as.character(pin),
+         pin = str_pad(pin, 13, "left"))
 
 
+
+access_db <- read_excel("incentivePINs_accessDB.xlsx") # %>% arrange((Status_cleaned) ) 
+
+
+# access_db <- access_db %>%  
+#   mutate(keypin = ifelse(`Concat PIN` %in% keypins, 1, 0)) %>%
+#   select(keypin, `Concat PIN`, everything())
+
+first_pins <- access_db %>% filter(Status_cleaned == "Approved") %>%
+  group_by(CONTROL) %>%
+  mutate(keypin = first(`Concat PIN`),
+  ) %>% mutate(pin = `Concat PIN`) %>% 
+  select(pin, keypin, CONTROL)
+
+incentive_pins <- left_join(incentive_pins, first_pins, by = c("pin" = "pin") )
+
+incentive_pins %>% group_by(CONTROL) %>% summarize(pin_count = n()) %>% arrange(-pin_count)
+# 1,284 incentive projects
+incentive_pins %>% group_by(keypin) %>% summarize(pin_count = n()) %>% arrange(-pin_count)
+# 1,253 incentive projects based on keypins.
+
+
+commerc_prop_keys <- read_csv("./Necessary_Files/Assessor_-_Commercial_Valuation_Data_20240212.csv") %>%
+  filter(year == 2021 #& !is.na(pins)
+         ) %>% 
+  select(clean_pin, clean_keypin, keypin, pins, `class(es)`) %>%
+  mutate(
+   # clean_pin = as.character(clean_pin),
+   # clean_pin = str_pad(clean_pin, 13, "left"),
+   # keypin = as.character(keypin),
+   keypin = gsub("-","", keypin),
+   keypin = gsub(" ","", keypin),
+    keypin = str_pad(keypin, 13, "left"),
+   clean_keypin = gsub("-","", clean_keypin),
+   
+    clean_keypin = gsub(" ","", clean_keypin),
+    classes = as.character(`class(es)`)
+    #kingpin = str_pad(kingpin, 13, "left"),
+  ) %>% 
+  arrange(desc(clean_keypin))
+
+keypins <- unique(commerc_prop_keys$clean_keypin)
+#cleanpins <- unique(commerc_prop_keys$clean_pin)
+#joined_dbs <- inner_join(commerc_prop_keys, incentive_pins, by = c("kingpin" = "pin") )
+
+incentive_pins <- incentive_pins %>% 
+  mutate(keypin_com_props = ifelse(pin %in% keypins, 1, 0),
+         #kingpin = ifelse(pin %in% cleanpins, 1, 0),
+         keypin_com_props2 = ifelse(pin ==keypin, 1, 0)) %>%
+  select(year,CONTROL, keypin, keypin_com_props, keypin_com_props2, pin, everything())
+
+table(incentive_pins$keypin_com_props)
+table(incentive_pins$keypin_com_props2)
+
+
+
+
+#joined_dbs <- left_join(incentive_pins, commerc_prop_keys, by = c("pin" = "clean_keypin") ) %>%
+ # select(year, clean_keypin, pin, everything())
+
+
+ 
 
 leftjoin_pins <- left_join(incentive_pins, access_db, by= c("pin" = "Concat PIN")) %>%
   select(pin, Status, `Start Year`, class, Notes )
@@ -21,9 +85,8 @@ leftjoin_pins <- left_join(incentive_pins, access_db, by= c("pin" = "Concat PIN"
 # 4146 unique pins. Left join adds duplicate entries from Access files 
 # 
 # two different status entries creates additional rows when joined
-leftjoin_pins %>%
-  distinct(pin) %>%
-  count()
+incentive_pins %>%
+  distinct(pin)
 
 # 3984  
 innerjoin_pins <- inner_join(incentive_pins, access_db, by= c("pin" = "Concat PIN")) %>%
@@ -32,10 +95,5 @@ innerjoin_pins <- inner_join(incentive_pins, access_db, by= c("pin" = "Concat PI
 innerjoin_pins 
 
 nojoin_pins <- anti_join(incentive_pins, access_db, by= c("pin" = "Concat PIN"))
-# 457 incentive PINs from 2021 are not found in the Access files w/o 7C, C, & L
-
-# 162 PINs not in Access
-
-
-nojoin_pins2 <- anti_join(access_db, incentive_pins, by= c("Concat PIN" = "pin"))
+# 162 PINs not in Access incentive PINs from 2021 are not found in the Access
 
