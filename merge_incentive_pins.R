3c338ac63087de7ac2df419f1f5b5b0e91ef63e6
AWM <awilbu2@uic.edu>
2024-02-23 10:37:39 -0600

    break up commercial and industrial properties. identify incentive PINs that existed each year

--- merge_incentive_pins.R
+++ merge_incentive_pins.R
@@ -20,6 +20,41 @@ incentive_pins <- read_csv("./Output/7_output_incentive_classes.csv") %>%
          block = str_sub(pin, 1, 7))  
 
 
+# Incentive PINs Each Year ------------------------------------------------
+
+library(ptaxsim)
+library(DBI)
+library(glue)
+
+ptaxsim_db_conn <- DBI::dbConnect(RSQLite::SQLite(), "./ptaxsim.db/ptaxsim-2022.0.0.db")
+
+cook_pins <- DBI::dbGetQuery(
+  ptaxsim_db_conn,
+  glue_sql(
+  "SELECT DISTINCT *
+  FROM pin
+  ",
+  .con = ptaxsim_db_conn
+  ))
+
+
+cook_pins <- cook_pins %>% filter(class > 599 & class < 900)
+cook_pins %>% group_by(year) %>% summarize(incentive_count = n())
+
+
+pin_classes <- cook_pins %>% group_by(pin, class) %>% distinct()
+
+cook_pins %>% group_by(pin) %>% summarize(count = n()) %>% arrange(-count)
+cook_pins %>% group_by(pin) %>% summarize(count = n()) %>% filter(count > 16)
+# 632 PINs have existed AND been incentive properties for all years in database.
+
+
+cook_pins %>% 
+  pivot_wider(id_cols = c(pin), names_from = "year", values_from =  "class") %>%
+  mutate(change = as.numeric(`2021`)-as.numeric(`2006`)) %>% 
+  filter(change !=0
+  )
+
 
 # CMAP DATABASE -----------------------------------------------------------
 # combined PIN tables into 1 file
@@ -31,62 +66,107 @@ access_db <- read_excel("incentivePINs_accessDB_2.xlsx")
 
 
 # create a keypin variable based on smallest PIN to compare to keypin used by assessor
-first_pins <- access_db %>% 
+# ideally, each CONTROL variable would have its own Key PIN.
+access_db <- access_db %>% 
   arrange(Status_cleaned,`Concat PIN`) %>%
-  group_by(CONTROL) %>% 
-  mutate( keypin = first(`Concat PIN`),
+  group_by(CONTROL, Status_cleaned) %>% 
+  mutate(keypin = first(`Concat PIN`),
          pin = `Concat PIN`,
-         n_PINs_inControl = n(),
-         class = first(Class)
+         n_PINs_inControlGroup = n(),  # Can include the same PIN multiple times!! 
+         class = first(Class),
+         parcel = str_sub(pin, 1, 10),
+         block = str_sub(pin, 1, 7)
          ) %>% 
-  select(pin, 
-         keypin, CONTROL, n_PINs_inControl, class) %>% 
-  arrange(CONTROL, keypin, pin,)
+  select(pin, keypin, CONTROL, n_PINs_inControlGroup, class, Status_cleaned, parcel, block) %>% 
+  arrange(CONTROL, keypin, pin)
+
+## Good start of making access database comparable to keypin database of assessor valuation data
 
 
 ## Join PTAXSIM incentive properties to CMAP incentive data
 # CMAP incentive PINs appear multiple times in database
-incentive_pins <- left_join(incentive_pins, first_pins, by = c("pin" = "pin") )
+incentive_pins <- left_join(incentive_pins, access_db, by = c("pin" = "pin") )
 
 incentive_pins %>% group_by(CONTROL) %>% summarize(pin_count = n()) %>% arrange(-pin_count)
 # 1,284 incentive projects
+# 2149 obs
 
 incentive_pins %>% group_by(keypin) %>% summarize(pin_count = n()) %>% arrange(-pin_count)
 # 1,253 incentive projects based on keypins.
+# 1,967 obs
 
 
-commerc_prop_keys <- read_csv("./Necessary_Files/Assessor_-_Commercial_Valuation_Data_20240212.csv") 
+# 
+# # Commercial Valuation Dataset - Cook County Data Portal ------------------
+# 
+# commerc_prop_keys <- read_csv("./Necessary_Files/Assessor_-_Commercial_Valuation_Data_20240212.csv") 
+# 
+# commerc_prop_keys <- commerc_prop_keys%>%
+#  # filter(#year == 2021 #& !is.na(pins)
+#   #       ) %>% 
+#   select(clean_pin, clean_keypin, keypin, pins, `class(es)`) %>%
+#   mutate(
+#     first_digit = str_sub(`class(es)`, 1, 1),
+#    # clean_pin = as.character(clean_pin),
+#    # clean_pin = str_pad(clean_pin, 13, "left"),
+#    # keypin = as.character(keypin),
+#    keypin = gsub("-","", keypin),
+#    keypin = gsub(" ","", keypin),
+#    keypin = str_pad(keypin, 13, "left"),
+#    clean_keypin = gsub("-","", clean_keypin),
+#    
+#     clean_keypin = gsub(" ","", clean_keypin),
+#     classes = as.character(`class(es)`),
+#    first_digit = str_sub(`class(es)`, 1, 1),
+#    
+#     #kingpin = str_pad(kingpin, 13, "left"),
+#   ) %>% 
+#   arrange(desc(clean_keypin))
 
-commerc_prop_keys <- commerc_prop_keys%>%
- # filter(#year == 2021 #& !is.na(pins)
-  #       ) %>% 
-  select(clean_pin, clean_keypin, keypin, pins, `class(es)`) %>%
-  mutate(
-    first_digit = str_sub(`class(es)`, 1, 1),
-   # clean_pin = as.character(clean_pin),
-   # clean_pin = str_pad(clean_pin, 13, "left"),
-   # keypin = as.character(keypin),
-   keypin = gsub("-","", keypin),
-   keypin = gsub(" ","", keypin),
-   keypin = str_pad(keypin, 13, "left"),
-   clean_keypin = gsub("-","", clean_keypin),
-   
-    clean_keypin = gsub(" ","", clean_keypin),
-    classes = as.character(`class(es)`),
-   first_digit = str_sub(`class(es)`, 1, 1),
-   
-    #kingpin = str_pad(kingpin, 13, "left"),
-  ) %>% 
-  arrange(desc(clean_keypin))
 
+# Commercial Valuation Dataset - Cook County Data Portal ------------------
 
 ## Manually selected all property options that began with a 6, 7, or 8, for any year (2021, 2022, and 2023)
 ## using online Cook County data portal
 prefiltered <- readxl::read_excel("./Necessary_Files/Manual_filter_assessorscommercialproperty.xlsx")
 
-keypins <- unique(commerc_prop_keys$clean_keypin)
-#cleanpins <- unique(commerc_prop_keys$clean_pin)
-#joined_dbs <- inner_join(commerc_prop_keys, incentive_pins, by = c("kingpin" = "pin") )
+prefiltered <- prefiltered %>% 
+  mutate(keypin_concat = as.character(keypin_concat), 
+   keypin_concat = str_pad(keypin_concat, 14, "left", pad = "0"))  %>%
+  select(keypin_concat:`class(es)`)
+
+# #### Assign Unique ID ######
+# library(data.table)
+# 
+# setDT(prefiltered)[, Unique_ID := .GRP, by = keypin_concat]
+# prefiltered <- prefiltered %>% arrange(keypin_concat)
+# 
+# prefiltered$pin = 1
+# 
+# nrow(prefiltered) # 306
+# 
+# for (i in 1:306){
+#   prefiltered$pin_num[[i + 1]] = ifelse(prefiltered$Unique_ID[[i + 1]] != prefiltered$Unique_ID[[i]], 1, 
+#                                  (prefiltered$pin_num[[i]] + 1))
+# }
+
+table(prefiltered$year)
+
+table(prefiltered$`class(es)`)
+
+
+keypins <- unique(prefiltered$keypin_concat)
+# 306 unique keypins from CCAO property valuation
+
+
+joined_dbs <- anti_join(prefiltered, incentive_pins, by = c("keypin_concat" = "pin") )
+
+pins_pivot<- prefiltered %>% select(keypin_concat, pins) %>%
+  muatate(pin[i] = str_split(pins, pattern = ","))
+
+  pivot_wider(id_cols = keypin_concat, values_from = "pins" , names_from = "pins", 
+              names_sep = ",")
+
 
 incentive_pins <- incentive_pins %>% 
   mutate(keypin_com_props = ifelse(pin %in% keypins, 1, 0),
