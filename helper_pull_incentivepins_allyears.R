d092528074da3119b7fca8fe78ccb5777e32a48f
Michael <mvanhu2@uic.edu>
2024-02-25 15:59:32 -0600

    Update to incorporate years incentive PINs existed without incentive classifications.

--- helper_pull_incentivepins_allyears.R
+++ helper_pull_incentivepins_allyears.R
@@ -1,15 +1,18 @@
 library(tidyverse)
 library(ptaxsim)
-library(DBI)
+library(DBI) #Guess we don't need the "here" package anymore.
 library(glue)
 
+# Instantiate and Test DB connection.
 
 ptaxsim_db_conn <- DBI::dbConnect(RSQLite::SQLite(), "./ptaxsim.db/ptaxsim-2022.0.0.db")
 
 
-## Pulls ALL distinct PINs that existed, ever. 
+
+## Pulls ALL distinct PINs that existed, ever.
 ## Takes a while to run.
 ## 31+ million obs. (PIN-YEAR combos)
+
 cook_pins <- DBI::dbGetQuery(
   ptaxsim_db_conn,
   glue_sql(
@@ -19,8 +22,18 @@ cook_pins <- DBI::dbGetQuery(
   .con = ptaxsim_db_conn
   ))
 
+ever_incent <- cook_pins %>%
+  filter(between(class, ))
+)
+
+ever_incent_pins <- as.list(ever_incent)
+
+# MVH NOTE: OLD CODE ONLY PULLED INCENTIVE PINs FOR YEARS WHERE THEY HAD
+# INCENTIVE CLASSIFICATIONS REVISED CODE PULLS ALL PINs THAT EVER HAD AN
+# INCENTIVE CLASSIFICATION AT ANY TIME.
+
 # keep only incentive properties : 600-899 class properties
-cook_pins <- cook_pins %>% filter(class > 599 & class < 900)
+#cook_pins <- cook_pins %>% filter(class > 599 & class < 900)
 # 45,724 obs. (PIN-YEAR combos)
 
 write_csv(cook_pins, "./Output/incentivePINs_allyears.csv")
