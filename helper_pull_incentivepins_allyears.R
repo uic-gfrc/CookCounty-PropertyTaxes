0351eca314f8ba3cc2b517992346bf673d1ecf8c
Michael <mvanhu2@uic.edu>
2024-02-25 22:36:01 -0600

    Write all obs. of PINs that were ever an incentive major class to csv.

--- helper_pull_incentivepins_allyears.R
+++ helper_pull_incentivepins_allyears.R
@@ -1,16 +1,14 @@
 library(tidyverse)
 library(ptaxsim)
-library(DBI) #Guess we don't need the "here" package anymore.
+library(DBI) #Guess we don't need the "here" package anymore?
 library(glue)
 
-# Instantiate and Test DB connection.
+# Instantiate DB connection.
 
 ptaxsim_db_conn <- DBI::dbConnect(RSQLite::SQLite(), "./ptaxsim.db/ptaxsim-2022.0.0.db")
 
-
-
 ## Pulls ALL distinct PINs that existed, ever.
-## Takes a while to run.
+## Takes a while to run. (~1 min w/ 64GB RAM)
 ## 31+ million obs. (PIN-YEAR combos)
 
 cook_pins <- DBI::dbGetQuery(
@@ -22,18 +20,30 @@ cook_pins <- DBI::dbGetQuery(
   .con = ptaxsim_db_conn
   ))
 
+# Change to numeric.
+
+cook_pins <- cook_pins %>%
+  mutate(class = as.numeric(class))
+
+## Limit to just incentive major classes (6, 7, 8)
+## ~46k obs (PIN-YEAR combos)
+
 ever_incent <- cook_pins %>%
-  filter(between(class, ))
-)
+  filter(between(class, 600, 899)) %>%
+  select(pin) %>%
+  distinct()
 
-ever_incent_pins <- as.list(ever_incent)
+ever_incent_pins <- as.list(ever_incent$pin)
 
-# MVH NOTE: OLD CODE ONLY PULLED INCENTIVE PINs FOR YEARS WHERE THEY HAD
-# INCENTIVE CLASSIFICATIONS REVISED CODE PULLS ALL PINs THAT EVER HAD AN
-# INCENTIVE CLASSIFICATION AT ANY TIME.
+## But we want those PINs as obs. for all years, even if they weren't classified
+## as an incentive in that time.
 
-# keep only incentive properties : 600-899 class properties
-#cook_pins <- cook_pins %>% filter(class > 599 & class < 900)
-# 45,724 obs. (PIN-YEAR combos)
+## Use incentive PIN list to get all obs.
+
+incent_pins <- cook_pins %>%
+  filter(pin %in% ever_incent_pins)
+
+## Write CSV to Output Folder
 
 write_csv(cook_pins, "./Output/incentivePINs_allyears.csv")
+
