d51156a40fd5cf3542cf57cffcbdb2db5deb3284
mvanhu2 <mvanhu2@uic.edu>
2024-04-13 12:05:11 -0500

    Generate visualization for Rachel of relationship between total, exe, and inc ptax rate changes.

---
+++ Rachel_4_19_Presentation.R
@@ -0,0 +1,47 @@
+### Materials for Rachel Weber's 4/19/24 Slide Deck ###
+### MVH 4-12-24 ###
+
+# Load packages and data
+
+options(digits = 2, scipen = FALSE)
+
+library(tidyverse)
+
+rates <- read_csv("alt_rates.csv")
+
+# Slide 9
+# Goal: scatterplot where change in tax rate from exemptions on x-axis and change from incentives on y-axis
+# Dot size represents total tax rate change
+# Another color dot will represent municipalities
+
+## Generate data
+
+delta_rates <- rates %>%
+  select(clean_name, delta_exe = change_noExe, delta_inc = change_noInc, delta_both = change_neither) %>%
+  distinct() %>%
+  arrange(desc(delta_both)) %>%
+  mutate(top_both = ifelse(row_number() %in% 1:20, 1, 0)
+  ) %>%
+  arrange(desc(delta_exe)) %>%
+  mutate(top_exe = ifelse(row_number() %in% 1:20, 1, 0)) %>%
+  arrange(desc(delta_inc)) %>%
+  mutate(top_inc = ifelse(row_number() %in% 1:20, 1, 0
+                          )) %>%
+  mutate(key_obs = ifelse(top_exe == 1 & top_inc == 1, 1, 0)) %>%
+  mutate(key_obs = as.factor(key_obs))
+
+## Create plot
+
+delta_rates %>%
+  ggplot(aes(delta_exe, delta_inc)) +
+  geom_point(aes(size = delta_both, color = key_obs), alpha = .5) +
+  scale_size_continuous(guide = "none") +
+  scale_color_manual(guide = "none", values = c("green", "orange")) +
+  labs(
+    x = "Exemptions",
+    y = "Incentives",
+    caption = "Observation size represents total change in tax rate from eliminating exemptions and incentives. Observations in orange represent municipalities with the top 20 rate changes from both exemptions and incentives."
+  ) +
+  theme_classic()
+
+
