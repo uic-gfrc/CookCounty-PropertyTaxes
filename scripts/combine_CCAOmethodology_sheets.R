42cd7a7c6a7184beafc67727934b4e720c9af01a
AWM <awilbu2@uic.edu>
2024-05-02 22:26:29 -0500

    combines north and south triads into own files. Attempts to do initial attempt at removing rows with only missing values and tries to  clean names before combining work sheets and excel files.

--- scripts/combine_CCAOmethodology_sheets.R
+++ scripts/combine_CCAOmethodology_sheets.R
@@ -1,7 +1,8 @@
 # Required libraries
 library(tidyverse)
 library(readxl)
-
+library(janitor)
+library(naniar)
 
 # Helper function to read sheets
 read_sheets <- function(file_path) {
@@ -13,79 +14,143 @@ read_sheets <- function(file_path) {
   dfs <- map_dfr(sheets, function(sheet) {
     read_excel(file_path, sheet = sheet) %>%
       mutate(sheet_name = sheet, file_name = basename(file_path)) %>%
+      
+    clean_names() %>%  # Clean variable names using janitor::clean_names()
+    #  map(~ .[, order(names(.))])  %>%   # Rearrange columns alphabetically
      mutate(across(everything(), as.character))
   })
   
   return(dfs)
 }
 
+# Deleted a summary table for affordable housing that was throwing off the  column names. 
+# deleted from Hanover
+
+# delete sheets 4 from Hanover
+
 
 # Main script
-dir_path <- "inputs/methodologyreports/"   # Path to the directory containing the files
+dir_path_north <- "inputs/methodologyreports/north_2022"   # Path to the directory containing the files
 
 # Get the list of files
-file_paths <- list.files(dir_path, pattern = "*.xlsx", full.names = TRUE)
-
+file_paths <- list.files(dir_path_north, pattern = "*.xlsx", full.names = TRUE)
 # Combine data from all files
-final_df <- map_dfr(file_paths, read_sheets)
+final_df_north <- map_dfr(file_paths, read_sheets) 
 
-# Output or further processing...
-# For example, save to a CSV file
-#write_csv(final_df, "combined_data.csv")
+final_df_north <- final_df_north %>% select(-c(x18:x29, x30:x32))
 
-library(naniar)
-library(janitor)
-# Rename variables using rename() function
+final_df_north <- final_df_north %>%
+  replace_with_na_all(~.x == "NA" )
+
+missing <- is.na(final_df_north$key_pin) | is.na(final_df_north$pi_ns  )
+# gets rid of 300,000+ rows that were messed up from reading in the excel files.
+final_df_north <- subset(final_df_north, subset = !missing)
+
+
+write.csv(final_df_north, "Output/combined_methodologyworksheets_NORTH.csv")
+
+
+
+dir_path_south <- "inputs/methodologyreports/south_2023"   # Path to the directory containing the files
+file_paths <- list.files(dir_path_south, pattern = "*.xlsx", full.names = TRUE
+                         )
+
+# Old way of combining files:
+# works, but doesn't clean variables before combining 
+ final_df_south <- map_dfr(file_paths, read_sheets)
+#final_df_south
+
+ 
+ final_df_south <- final_df_south %>%
+   replace_with_na_all(~.x == "NA" )
+ 
+ 
+ 
+ 
+write.csv(final_df_south, "Output/combined_methodologyworksheets_SOUTH.csv")
+
+ 
+ 
+ 
+  
+# # Load all Excel files into a list of data frames
+# load_clean_rearrange <- function(file) {
+#   excel_sheets(file) %>%
+#     set_names() %>%
+#     map(read_excel, path = file) %>%
+#     map(clean_names) %>%  # Clean variable names using janitor::clean_names()
+#     map(~ .[, order(names(.))])  %>%   # Rearrange columns alphabetically
+#     map(~ mutate_all(., as.character))  # Convert all variables to character type
+# }
+# 
+# data_list <- lapply(file_paths, load_clean_rearrange)
+#  
+# all_column_names <- unique(unlist(unlist(lapply(data_list, names))))
+# 
+# 
+# # Function to ensure all data frames have the same set of columns
+# standardize_columns <- function(df) {
+#   missing_columns <- setdiff(all_column_names, names(df))
+#   df[, missing_columns] <- NA
+#   df <- df[, all_column_names]
+#   return(df)
+# }
+# 
+
+# combined_data <- data_list %>%
+#   bind_rows(.id = "source_file")
+# # Output or further processing...
+# # For example, save to a CSV file
+# #write_csv(final_df, "combined_data.csv")
+
 
 
 ## 14,575 obs after importing South Triad excel files. 
 
 
-final_df <- final_df %>%
-  replace_with_na_all(~.x == "NA" )  %>% 
-  clean_names()
 
-final_df %>% write_csv("Output/CCAOmethodologyreports_temp.csv")
-
-data_renamed <- final_df %>%
-  mutate(
-    
-    adj_rent_sf = ifelse(is.na(adj_rent_sf), adj_rent_sf_2, adj_rent_sf),
-    bldg_sqft = ifelse(is.na(bldg_sqft), bldg_sq_ft, bldg_sqft ),
-    bldg_sqft = ifelse(is.na(bldg_sqft), bldg_sf, bldg_sqft ),
-    excess_land_area = ifelse(is.na(excess_land_area), excess_land_area_2, excess_land_area),
-
-    excess_land_value = ifelse(is.na(excess_land_value), excess_land_value_2, excess_land_value),
-    excess_land_value = ifelse(is.na(excess_land_value), excess_land_value_3, excess_land_value),
-   # final_market_value = ifelse(is.na(final_market_value), final_market_value_2, final_market_value),
-    final_mv_sf = ifelse(is.na(final_mv_sf), final_mv_sf_2, final_mv_sf),
-    mobile_home_pads = ifelse(is.na(mobile_home_pads), mobile_home_pads_2, mobile_home_pads),
-    pct_owner_interest = ifelse(is.na(pct_owner_interest), pct_owner_interest_2, pct_owner_interest),
-    percent_exp = ifelse(is.na(percent_exp), percent_exp_2, percent_exp),
-    property_description = ifelse(is.na(property_description), property_description_2, property_description),
-    total_land_sf = ifelse(is.na(total_land_sf), total_land_sf_2, total_land_sf),
-   total_exp = ifelse(is.na(total_exp), total_exp_2, total_exp),
-   
-   market_value = ifelse(is.na(market_value), market_value_2, market_value),
-   x1br_units = ifelse(is.na(x1br_units), x1br_units_2, x1br_units),
-   x2br_units = ifelse(is.na(x2br_units), x2br_units_2, x2br_units),
-   x3br_units = ifelse(is.na(x3br_units), x3br_units_2, x3br_units),
-   x2023_permit_partial_demo_value = ifelse(is.na(x2023_permit_partial_demo_value), x2023_permit_partial_demo_value_2, x2023_permit_partial_demo_value),
-   vacancy_percent = ifelse(is.na(vacancy_percent), percent_vac, vacancy_percent),
-   year_built = ifelse(is.na(year_built), year_built_2, year_built),
-   year_built = ifelse(is.na(year_built), year_built_3, year_built),
-   final_mv_sf = ifelse(is.na(final_mv_sf), final_mv_sf_2, final_mv_sf) 
-   )     %>% 
-  select(-c(adj_rent_sf_2, final_mv_sf_2, year_built_2, year_built_3, total_exp_2, percent_exp_2, excess_land_value_2, excess_land_value_3, bldg_sq_ft, bldg_sf, excess_land_value_2, excess_land_value_3, market_value_2,
-            property_description_2, pct_owner_interest_2, mobile_home_pads_2, x2023_permit_partial_demo_value_2, x1br_units_2, x2br_units_2, x3br_units_2 )) %>%
-  select(pin = ias_world_pi_ns, key_pin, classes, sheet_name, file_name, market_value ,everything()) %>%
-  mutate(across(.cols = c(adj_rent_sf:percent_exp, total_land_sf:year_built), .fns = as.numeric)) 
-
-
+#final_df %>% write_csv("Output/CCAOmethodologyreports_temp.csv")
+# 
+# data_renamed <- final_df %>%
+#   mutate(
+#     
+#     adj_rent_sf = ifelse(is.na(adj_rent_sf), adj_rent_sf_2, adj_rent_sf),
+#     bldg_sqft = ifelse(is.na(bldg_sqft), bldg_sq_ft, bldg_sqft ),
+#     bldg_sqft = ifelse(is.na(bldg_sqft), bldg_sf, bldg_sqft ),
+#     excess_land_area = ifelse(is.na(excess_land_area), excess_land_area_2, excess_land_area),
+# 
+#     excess_land_value = ifelse(is.na(excess_land_value), excess_land_value_2, excess_land_value),
+#     excess_land_value = ifelse(is.na(excess_land_value), excess_land_value_3, excess_land_value),
+#    # final_market_value = ifelse(is.na(final_market_value), final_market_value_2, final_market_value),
+#     final_mv_sf = ifelse(is.na(final_mv_sf), final_mv_sf_2, final_mv_sf),
+#     mobile_home_pads = ifelse(is.na(mobile_home_pads), mobile_home_pads_2, mobile_home_pads),
+#     pct_owner_interest = ifelse(is.na(pct_owner_interest), pct_owner_interest_2, pct_owner_interest),
+#     percent_exp = ifelse(is.na(percent_exp), percent_exp_2, percent_exp),
+#     property_description = ifelse(is.na(property_description), property_description_2, property_description),
+#     total_land_sf = ifelse(is.na(total_land_sf), total_land_sf_2, total_land_sf),
+#    total_exp = ifelse(is.na(total_exp), total_exp_2, total_exp),
+#    
+#    market_value = ifelse(is.na(market_value), market_value_2, market_value),
+#    x1br_units = ifelse(is.na(x1br_units), x1br_units_2, x1br_units),
+#    x2br_units = ifelse(is.na(x2br_units), x2br_units_2, x2br_units),
+#    x3br_units = ifelse(is.na(x3br_units), x3br_units_2, x3br_units),
+#    x2023_permit_partial_demo_value = ifelse(is.na(x2023_permit_partial_demo_value), x2023_permit_partial_demo_value_2, x2023_permit_partial_demo_value),
+#    vacancy_percent = ifelse(is.na(vacancy_percent), percent_vac, vacancy_percent),
+#    year_built = ifelse(is.na(year_built), year_built_2, year_built),
+#    year_built = ifelse(is.na(year_built), year_built_3, year_built),
+#    final_mv_sf = ifelse(is.na(final_mv_sf), final_mv_sf_2, final_mv_sf) 
+#    )     %>% 
+#   select(-c(adj_rent_sf_2, final_mv_sf_2, year_built_2, year_built_3, total_exp_2, percent_exp_2, excess_land_value_2, excess_land_value_3, bldg_sq_ft, bldg_sf, excess_land_value_2, excess_land_value_3, market_value_2,
+#             property_description_2, pct_owner_interest_2, mobile_home_pads_2, x2023_permit_partial_demo_value_2, x1br_units_2, x2br_units_2, x3br_units_2 )) %>%
+#   select(pin = ias_world_pi_ns, key_pin, classes, sheet_name, file_name, market_value ,everything()) %>%
+#   mutate(across(.cols = c(adj_rent_sf:percent_exp, total_land_sf:year_built), .fns = as.numeric)) 
+# 
+# 
+# write.csv(data_renamed, "Output/combined_methodologyworksheets.csv")
 
 
-write.csv(data_renamed, "Output/combined_methodologyworksheets.csv")
 
+# Unnesting -------------------------------------------------------
 data_renamed_unnest <- data_renamed %>% 
   
   mutate(keypin_concat = as.character(key_pin),
