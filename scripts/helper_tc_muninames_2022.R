1aa5a8002600144d3717f2a3cc2f998d6a1ec791
mvanhu2 <mvanhu2@uic.edu>
2024-05-27 17:41:59 -0500

    Remove unnecessary packages.

--- scripts/helper_tc_muninames_2022.R
+++ scripts/helper_tc_muninames_2022.R
@@ -5,10 +5,8 @@
 library(tidyverse)
 library(DBI)
 library(data.table)
-library(gstat)
 library(here)
 library(httr)
-#library(jsonlite)
 library(ptaxsim)
 library(glue)
 
@@ -25,14 +23,14 @@ nicknames <- readxl::read_excel("./Necessary_Files/muni_shortnames.xlsx")
 
 
 
-## `agency_dt` has all taxing agencies (but not TIFs) that existed each year and 
-## includes their total taxable base (cty_cook_eav), their levy, taxing rate, 
-## binary variables for if a municipality is home rule or not, as well as many 
-## other variables. 
+## `agency_dt` has all taxing agencies (but not TIFs) that existed each year and
+## includes their total taxable base (cty_cook_eav), their levy, taxing rate,
+## binary variables for if a municipality is home rule or not, as well as many
+## other variables.
 
-## tax_bill() uses this table for the taxable EAV that is used 
-## to  calculate the tax rates in the tax bills. For simulations, you must alter 
-## the taxable EAV or levy or other variables and then tell tax_bill() function 
+## tax_bill() uses this table for the taxable EAV that is used
+## to  calculate the tax rates in the tax bills. For simulations, you must alter
+## the taxable EAV or levy or other variables and then tell tax_bill() function
 ## to use the modified agency data table for simulated tax bills.
 
 
@@ -60,7 +58,7 @@ muni_agency_names <- DBI::dbGetQuery(
   "SELECT DISTINCT agency_num, agency_name, minor_type
   FROM agency_info
   WHERE minor_type = 'MUNI'
-  OR agency_num = '020060000'  
+  OR agency_num = '020060000'
   "
 )
 
@@ -76,28 +74,28 @@ muni_tax_codes <- DBI::dbGetQuery(
   ",
   .con = ptaxsim_db_conn
   )
-)  
+)
 
 
-#  ## has all tax codes and the composite tax rate for the tax code 
+#  ## has all tax codes and the composite tax rate for the tax code
 tax_codes <- DBI::dbGetQuery(
   ptaxsim_db_conn,
   glue_sql("
   SELECT DISTINCT tax_code_num, tax_code_rate
   FROM tax_code
-  WHERE year = 2022  
+  WHERE year = 2022
   ",
   .con = ptaxsim_db_conn
   )
 )
 
-## All tax codes. 
-## tax codes within municipalities have additional info 
-tc_muninames <- tax_codes %>% 
+## All tax codes.
+## tax codes within municipalities have additional info
+tc_muninames <- tax_codes %>%
   left_join(muni_tax_codes) %>%
-  left_join(muni_agency_names) %>% 
-  select(-agency_rate) %>% 
-  left_join(nicknames) %>% 
-  select(-c(minor_type, short_name, 
-           # `Column1`, `Most recent reassessed`, 
+  left_join(muni_agency_names) %>%
+  select(-agency_rate) %>%
+  left_join(nicknames) %>%
+  select(-c(minor_type, short_name,
+           # `Column1`, `Most recent reassessed`,
             agency_number))
